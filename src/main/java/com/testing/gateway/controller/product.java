bb5ec72bed707843c068ec50bbd628d69f26faea
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-09-01 15:25:59 +0700

    [FEATURE] Added ffeature for product controller

--- src/main/java/com/testing/gateway/controller/product.java
+++ src/main/java/com/testing/gateway/controller/product.java
@@ -4,8 +4,21 @@ import com.testing.gateway.entity.ProductResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/api/product")
 public class product {
-    public ProductResponse testReport()
+    public ProductResponse testReport(){
+        try{
+            return new ProductResponse(
+                    "AA001",
+                    LocalDateTime.now().toString(),
+                    true,
+                    "test report successful using spring cloud api gateway"
+            );
+        }catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
